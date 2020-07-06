e051e3e856cefcbe4c41e9c7a4575f7171769087
hekui <hekui@kuaishou.com>
2020-07-06 15:15:48 +0800

    初始化工程

---
+++ src/main/java/Demo.java
@@ -0,0 +1,9 @@
+/**
+ * @author hekui <hekui@kuaishou.com>
+ * Created on 2020-07-06
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
